9ec26ad3b713df07059716f1412406b3c141e47e
Junaid <junaid@Cayzens-MacBook-Air.local>
2020-09-11 11:41:10 +0530

    update

--- server.js
+++ server.js
@@ -16,5 +16,14 @@ app.use(express.json());
 app.use('/api/v1/posts',posts);
 app.use('/api/v1/comments',comments);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
